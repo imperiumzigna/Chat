9734bb54435ead530c1ca0eaebd1d7ff303e2f5a
LuizRicardoRipardo <ricardo_ripardo@hotmail.com>
2015-10-05 09:59:15 -0300

    Projeto Chat

---
+++ Chat/Scripts/chat.js
@@ -0,0 +1,15 @@
+﻿var carrega = function carregaMensagens() {
+
+    var ajax = $.get("ChatApi/ReceiveAll");
+    ajax.done(function receive(result) {
+        console.log(result);
+        for (var i = 0; i < result.lenght; i++) {
+            $("#conversas").append(result[i].Usuario);
+            console.log(result[i].Usuario);
+            for (var j = 0; j < result[i].Mensagens.lenght; j++) {
+                $("#conteudo").append(result[i].Mensagens[j].Usuario + ' diz às ' + new Date(conversas[i].Mensagens[j].Hora));
+                console.log(result[i].Mensagens[j]);
+            }
+        }
+    });
+}
