7cab144b5a22b62bc163633a073aedcaebd31c85
Igor Amorim Silva <igoramorimsilva2@hotmail.com>
2015-10-06 14:06:42 -0300

    Receber Mensagens Funcionando

--- Back End/Chat/Chat/Scripts/chat.js
+++ Back End/Chat/Chat/Scripts/chat.js
@@ -1,24 +1,35 @@
-﻿
-var carrega = function carregaMensagens() {
+﻿// Carrega as Mensagens da Base e mostra na tela.
+var carrega = function () {
 
-    var ajax = $.get("ChatApi/ReceiveAll");
-    ajax.done(function receive(result) {
-        console.log(result);
-        for (var i = 0; i < result.lenght; i++) {
-            $("#conversas").append(result[i].Usuario);
-            console.log(result[i].Usuario);
-            for (var j = 0; j < result[i].Mensagens.lenght; j++) {
-                $("#conteudo").append(result[i].Mensagens[j].Usuario + ' diz às ' + new Date(conversas[i].Mensagens[j].Hora));
-                console.log(result[i].Mensagens[j]);
-            }
-        }
+    var ajax = $.get("ChatApi/ReceiveAll", function (objeto) {
+        
+        $("#conversas").append("<p>" + objeto[0].Usuario + " diz:</p>");
+        $("#conteudo").append("<p>" + objeto[0].Mensagens[0].Conteudo + "</p>");
     });
+    
+    //ajax.done(function (data) {
+    //    console.log(data);
+
+    //    return data;
+    //});
+
+    //ajax.done(function receive(result) {
+        
+    //    for (var i = 0; i < result.lenght; i++) {
+    //        $("#conversas").append(result[i].Usuario.value);
+    //        console.log(result[i].Usuario);
+    //        for (var j = 0; j < result[i].Mensagens.lenght; j++) {
+    //            $("#conteudo").append(result[i].Mensagens[j].Usuario + ' diz às ' + new Date(conversas[i].Mensagens[j].Hora));
+    //            console.log(result[i].Mensagens[j]);
+    //        }
+    //    }
+    //    console.log(result);
+
+    //   return result
+    //});
+   
 }
 
 // Teste de busca pelo nome do usuário 
-//$(document).ready(
-//    function () {
-//        $("#buscar").submit(function () {
-//        alert($("#inpt_search").val());
-//        });
-//    });
+$(document).ready(
+carrega());
